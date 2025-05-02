aafec8a08820f97dd6518661b3fa4825903a0e26
ElvisNdunguMbuthia <maishamagicast@gmail.com>
2025-05-02 18:50:02 +0300

    passed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+function onFocus(){
+    console.log('Good!');
+}
+function onBlur(){
+    console.log('Hey! Eyes on me!');
+}
+function EyesOnMe() {
+    return(
+    <button onFocus={()=>{onFocus()}} onBlur={()=>{onBlur()}}>Eyes on me</button>
+)}
+
+
+export default EyesOnMe;
